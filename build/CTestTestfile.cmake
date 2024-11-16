bb19c3986d685a0823ddec04f5cd9dbdab30e0c3
SophaGekk <168344741+SophaGekk@users.noreply.github.com>
2024-11-16 19:40:00 +0700

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Sopha/Desktop/discretka/binary_tree
-# Build directory: C:/Users/Sopha/Desktop/discretka/binary_tree/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
